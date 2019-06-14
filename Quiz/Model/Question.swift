4fa8ef2b0a85a8bc23ee151cd01b38606265bcd9
Cristian <chris.sedano.arenas@gmail.com>
2019-06-14 12:21:53 +0100

    Implement Question.swift and QuizManager.swift
    inside Question.swift, implement struct, and QuizManager.swift, implement data, init, func generateNumber and func randomQuestion

---
+++ Quiz/Model/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Quiz
+//
+//  Created by Cristian Sedano Arenas on 14/06/2019.
+//  Copyright © 2019 Cristian Sedano Arenas. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var question: String
+    var options: [String]
+    var answer: Int
+}
---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Cristian Sedano Arenas on 14/06/2019.
+//  Copyright © 2019 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
